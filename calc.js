4010f5837ff3fe40276514a190ac79c565010ad9
Justin - PC <justinjoyce88@gmail.com>
2018-05-11 13:37:26 -0400

    syntax and small fixes

--- calc.js
+++ calc.js
@@ -1,33 +1,35 @@
+"use strict";
+
 $(() => {
   let entries = $(".entries");
-  let history = $('.history');
+  let history = $(".history");
 
-  $('.num-btn').on('click', function () {
+  $(".num-btn").on("click", function() {
     entries.append(entries.val() + $(this).html());
     history.append(history.val() + $(this).html());
   });
 
-  $('.math-btn').on('click', function () {
+  $(".math-btn").on("click", function() {
     entries.html("");
     entries.html($(this).html());
     history.append($(this).html());
   });
 
-  $('.all-clear').on('click', function () {
-    history.html('');
-    entries.html('');
+  $(".all-clear").on("click", function() {
+    history.html("");
+    entries.html("");
   });
 
-  $('.clear').on('click', function () {
+  $(".clear").on("click", function() {
     history.html(previous(history.html()));
-    entries.html('');
+    entries.html("");
   });
 
-  $('.equals').on('click', function () {
+  $(".equals").on("click", function() {
     let calc = math.eval(history.html());
     if (calc === Infinity) {
       entries.html("Cannot divide by zero");
-      history.html('');
+      history.html("");
     } else {
       history.html(calc);
       entries.html(calc);
@@ -35,16 +37,16 @@ $(() => {
   });
 });
 
-let previous = (str) => {
-  let arr = str.split('');
+const previous = str => {
+  let arr = str.split("");
   for (let i = arr.length - 1; i > 0; i--) {
-    let res = arr[i].match(/[+\-*\/]/g);
+    const res = arr[i].match(/[+\-*\/]/g);
     if (res) {
       arr.pop();
       break;
     }
     arr.pop();
   }
-  let newStr = arr.join('');
+  const newStr = arr.join("");
   return newStr;
 };
